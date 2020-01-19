412019a5b51837c114ee2fd0d573b34790c80f51
angelosolitario <nglsolitario377@gmail.com>
2020-01-18 20:09:20 -0800

    added icon for social medias

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./components/About";
 import Projects from "./components/About";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <div className="App">
-            Hello there!           
+            <About />
             <Switch>
                 <Route path="/"></Route>
                 <Route path="/about" component={About}></Route>
--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React from "react";
+import "../static/css/about.css";
+import profilepicture from "../static/img/profilepicture.JPG";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithub,
+    faLinkedin,
+    faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
-const About = () =>{
-
-    return(
+const About = () => {
+    return (
         <div>
-
-            <div>
+            <div className="container">
                 {/* This will be the profile(picture) */}
+                <img
+                    className="profile-picture"
+                    src={profilepicture}
+                    alt="angelo-solitario"
+                ></img>
+
+                <a href="https://github.com/angelosolitario">
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
+
+                <a href="https://linkedin.com/in/angelosolitario">
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
+
+                <a href="https://instagram.com/angelosolitario">
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+
                 <h1>Angelo Solitario</h1>
             </div>
+            <div className ="row">
+                <div className = "col">
+                    <h1>Education</h1>
+                    {/* This will be the education part */}
+                </div>
 
-            <div>
-                <h1>Education</h1>
-                {/* This will be the education part */}
+                <div className = "col">
+                    <h1>Skills</h1>
+                    {/* This will be the skills part */}
+                </div>
             </div>
-
-            <div>
-                <h1>Skills</h1>
-                {/* This will be the skills part */}
-            </div>
-
-
         </div>
-    )
-}
+    );
+};
 
 export default About;
