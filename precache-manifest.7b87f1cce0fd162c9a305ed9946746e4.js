052d5d20889d4813c30b0e7133b1d718f569da75
angelosolitario <nglsolitario377@gmail.com>
2020-06-24 22:09:52 -0700

    Updates

---
+++ precache-manifest.7b87f1cce0fd162c9a305ed9946746e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d3205b086c19dd8f7cf84eb664c5a3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "63baee8ab6cc0af0c3b0",
+    "url": "/portfolio/static/css/2.546755be.chunk.css"
+  },
+  {
+    "revision": "a9a447da359fa09160f8",
+    "url": "/portfolio/static/css/main.f7788322.chunk.css"
+  },
+  {
+    "revision": "63baee8ab6cc0af0c3b0",
+    "url": "/portfolio/static/js/2.98c1a1e9.chunk.js"
+  },
+  {
+    "revision": "4648310631ffed799e36605e1d4f0bae",
+    "url": "/portfolio/static/js/2.98c1a1e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9a447da359fa09160f8",
+    "url": "/portfolio/static/js/main.b8aaf7b4.chunk.js"
+  },
+  {
+    "revision": "24fff9536ad58a28fa4f",
+    "url": "/portfolio/static/js/runtime-main.a4c0ad0d.js"
+  },
+  {
+    "revision": "6fea5720e644f881481e9a68adde54b3",
+    "url": "/portfolio/static/media/home-page-gatorstore.6fea5720.png"
+  },
+  {
+    "revision": "1d7587c5115ad1de3a59e7fe2dc07edc",
+    "url": "/portfolio/static/media/profilepicture.1d7587c5.JPG"
+  },
+  {
+    "revision": "20ce18e8817d9f15fd36d516d64474bc",
+    "url": "/portfolio/static/media/todo-list.20ce18e8.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.85f38f4a235bd7ca56f9c2cb0860f4eb.js"
+  "/portfolio/precache-manifest.7b87f1cce0fd162c9a305ed9946746e4.js"
 );
 
 self.addEventListener('message', (event) => {
