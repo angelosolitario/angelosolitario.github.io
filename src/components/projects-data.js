692f185682fbaa5a6ccb71b7ea4af0c9055710a8
angelosolitario <nglsolitario377@gmail.com>
2020-06-30 21:44:11 -0700

    adding other projects to projects component

--- src/components/projects-data.js
+++ src/components/projects-data.js
@@ -1,5 +1,7 @@
 import HomePage from "../static/img/home-page-gatorstore.png";
 import TodoPage from "../static/img/todo-list.gif";
+import TankGame from '../static/img/tank-game.jpg'
+import NoteTaking from '../static/img/note-taking-app.png'
 const projects = [
     {
         title: "Gator Store",
@@ -21,6 +23,26 @@ const projects = [
             backend: "MongoDB, Node.js",
         },
         desc: "A Full Stack Todo List that allows users to add and delete an item.",
+    },
+    {
+        title: "Note Taking App",
+        image: NoteTaking,
+        link:"https://github.com/angelosolitario/NoteTakingApp",
+        tech: {
+            frontend: "HTML, CSS, JavaScript, React, Redux",
+            backend: "MongoDB, Express, Node.js",
+        },
+        desc: "A Note Taking application that allows users to login/register, add/delete/edit a note item.",
+    },
+    {
+        title: "Multiplayer Tank Game",
+        image: TankGame,
+        link:"https://github.com/angelosolitario/Multiplayer-Tank-Game",
+        tech: {
+            frontend: "Java",
+            backend: "Java",
+        },
+        desc: "A Multiplayer Tank Game using Java/GUI",
     },
 ];
 
