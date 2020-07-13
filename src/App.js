51108de513477e4fd78b69ee10975bd388db4be2
angelosolitario <nglsolitario377@gmail.com>
2020-07-12 23:29:29 -0700

    adding background on projects

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-
 function App() {
     return (
         <div className="App">
